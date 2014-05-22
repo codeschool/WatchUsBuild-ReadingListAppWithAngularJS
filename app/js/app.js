4ca7db5dff25bf80e6bc5c2385b6452e274a69b6
alyssamichelle <mail@alyssanicoll.com>
2014-05-22 10:18:30 -0400

    Added in new book data, yeah!

--- app/js/app.js
+++ app/js/app.js
@@ -51,26 +51,33 @@
   var genres = [ 'fable', 'fantasy', 'fiction', 'folklore', 'horror', 'humor', 'legend', 'metafiction', 'mystery', 'mythology', 'non-fiction', 'poetry' ];
 
   var books = [
-    { title: 'Pragmatic Thinking & Learning',
-      author: 'Andy Hunt',
-      isbn: '1934356050',
-      review: "Loved this book!",
+    { title: 'A Game of Thrones (A Song of Ice and Fire, Book 1)',
+      author: 'George R.R. Martin',
+      isbn: '0553593714',
+      review: 'The most inventive and entertaining fantasy saga of our time—warrants one hell of an introduction. I loved this book!',
       rating: 4,
-      genres: { horror: true }
+      genres: { 'non-fiction': true, fantasy: true }
     },
-    { title: 'The Age of Spiritual Machines',
-      authors: 'Ray Kurzweil',
-      isbn: '0140282025',
-      review: "Would not read again.",
-      rating: 2,
-      genres: { fable: true }
-    },
-    { title: 'Elven Star: The Death Gate Cycle',
-      authors: 'Margaret Weiss',
-      isbn: '0553290983',
-      review: "Best fantasy series EVAR",
+    { title: 'HTML for Babies',
+      authors: 'John C Vanden-Heuvel Sr',
+      isbn: '0615487661',
+      review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
       rating: 5,
-      genres: { fiction: true, fantasy: true }
+      genres: { fiction: true }
+    },
+    { title: 'A is for Array',
+      authors: 'Brandon J Hansen',
+      isbn: '1489522212',
+      review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
+      rating: 4,
+      genres: { fiction: true }
+    },
+    { title: 'The Dragon Reborn',
+      authors: 'Robert Jordan',
+      isbn: '0812513711',
+      review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
+      rating: 4,
+      genres: { 'non-fiction': true, fantasy: true }
     }
   ];
 })();
