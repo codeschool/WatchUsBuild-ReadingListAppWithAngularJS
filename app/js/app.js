6db1da410c4ebefc1fabe9a4b8a0493051827f1a
alyssamichelle <mail@alyssanicoll.com>
2014-05-29 14:54:27 -0400

    changed authors key to author in books data

--- app/js/app.js
+++ app/js/app.js
@@ -64,21 +64,21 @@
       genres: { 'non-fiction': true, fantasy: true }
     },{
       title: 'HTML for Babies',
-      authors: 'John C Vanden-Heuvel Sr',
+      author: 'John C Vanden-Heuvel Sr',
       isbn: '0615487661',
       review: "It's never too early to be standards compliant! I taught my little one mark-up in under one hour!",
       rating: 5,
       genres: { fiction: true }
     },{
       title: 'A is for Array',
-      authors: 'Brandon J Hansen',
+      author: 'Brandon J Hansen',
       isbn: '1489522212',
       review: 'A is for Array is the ABC book for future programmers. Filled with fun illustrations and simple real-world examples, my children loved seeing my world intertwined with theirs!',
       rating: 4,
       genres: { fiction: true }
     },{
       title: 'The Dragon Reborn',
-      authors: 'Robert Jordan',
+      author: 'Robert Jordan',
       isbn: '0812513711',
       review: 'The Wheel weaves as the Wheel wills, and we are only the thread of the Pattern. Moiraine',
       rating: 4,
